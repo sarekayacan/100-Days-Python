8160e5e5b509a12f1b2c1e547cd00882b5b5b807
Sare Kayacan <kayacansaree@gmail.com>
2026-01-02 00:54:15 +0300

    day49 : Global Weather Dashboard

--- day49.py
+++ day49.py
@@ -2,7 +2,7 @@
 import requests
 import matplotlib.pyplot as plt
 
-API_KEY = "bcf1017dfd543aac60fad940f1abf3c9"
+API_KEY = "YOURS_API_KEY"
 BASE_URL_CURRENT = "https://api.openweathermap.org/data/2.5/weather"
 BASE_URL_FORECAST = "https://api.openweathermap.org/data/2.5/forecast"
 
