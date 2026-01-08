cdc1e712e314123dd4feba395a6d6340c55e262f
Sare Kayacan <kayacansaree@gmail.com>
2026-01-08 19:57:10 +0300

    day86 : Support Vector Machines

--- day86.py
+++ day86.py
@@ -20,7 +20,7 @@ X = np.array([
 y = np.array([1, 0, 1, 0, 1, 1, 0, 1, 0, 1])
 
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42
+    X, y, test_size=0.4, random_state=42, stratify=y
 )
 
 model = SVC(kernel='linear')
